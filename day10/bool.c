c8a30d125800d7668031cd36fea02e067948a8dc
meenusakthivel <meenasakthivel13@gmail.com>
2025-05-22 15:44:45 +0530

    Add files via upload

---
+++ day10/bool.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool perfect(int x){
+    int sum =0;
+      for(int i=1;i<x;i++){
+       if(x%i==0){
+           sum=sum+i;
+       }
+   }
+      return sum==x;
+}
+int main() {
+   int x;
+   scanf("%d",&x);
+   bool a=perfect(x);
+ 
+  if(a)printf("true");
+  else printf("false");
+    return 0;
+}
